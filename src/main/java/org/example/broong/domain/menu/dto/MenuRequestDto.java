e502e46126634bc871b3e7c131b1f9cff3f9f410
KSH-a <kimjune.net@gmail.com>
2025-04-23 19:56:47 +0900

    Feat : MenuRequestDto, MenuResponseDto 작성

--- src/main/java/org/example/broong/domain/menu/dto/MenuRequestDto.java
+++ src/main/java/org/example/broong/domain/menu/dto/MenuRequestDto.java
@@ -1,4 +1,13 @@
 package org.example.broong.domain.menu.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MenuRequestDto {
+    private Long storeId;        // 가게 식별자
+    private String name;         // 메뉴 이름
+    private String price;        // 가격 (문자열로 저장 중)
+    private String menuState;    // 메뉴 상태 (예: AVAILABLE, DELETED 등)
 }
--- src/main/java/org/example/broong/domain/menu/dto/MenuResponseDto.java
+++ src/main/java/org/example/broong/domain/menu/dto/MenuResponseDto.java
@@ -1,4 +1,17 @@
 package org.example.broong.domain.menu.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MenuResponseDto {
+    private Long id;             // 메뉴 고유 ID
+    private Long storeId;        // 연결된 가게 ID
+    //private String storeName;   //연관관계 시 추가 가능
+    private String name;         // 메뉴 이름
+    private String price;        // 가격
+    private String menuState;    // 상태
+
+
 }
