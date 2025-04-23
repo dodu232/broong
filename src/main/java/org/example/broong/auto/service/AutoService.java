c97d73a82352ea509c11371c3b394d8470de07ba
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-23 17:50:09 +0900

    feat: AutoService 회원가입 기능 구현
    이메일 존재 여부  확인후 비밀번호 인코딩, usertype 확인 후 point 지급 및 token 생성

---
+++ src/main/java/org/example/broong/auto/service/AutoService.java
@@ -0,0 +1,51 @@
+package org.example.broong.auto.service;
+
+import static org.example.broong.global.exception.ErrorType.INVALID_PARAMETER;
+import lombok.RequiredArgsConstructor;
+import org.example.broong.auto.dto.request.SignupRequestDto;
+import org.example.broong.auto.dto.response.SignupResponseDto;
+import org.example.broong.config.JwtUtil;
+import org.example.broong.config.PasswordEncoder;
+import org.example.broong.global.exception.ApiException;
+import org.example.broong.user.entity.User;
+import org.example.broong.user.enums.UserType;
+import org.example.broong.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutoService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public SignupResponseDto signup(SignupRequestDto requestDto) {
+
+        if(userRepository.existsByEmail(requestDto.getEmail())){
+            throw new ApiException(HttpStatus.CONFLICT, INVALID_PARAMETER, "이미 존재하는 이메일 입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+
+        UserType userType = UserType.of(requestDto.getUserType());
+
+        User newUser = new User(
+                requestDto.getName(),
+                requestDto.getEmail(),
+                encodedPassword,
+                userType
+        );
+
+        newUser.setPoint(100);
+
+        User savedUser = userRepository.save(newUser);
+
+        String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(),
+                savedUser.getUserType());
+
+        return new SignupResponseDto(bearerToken);
+
+    }
+}
