5301344f2e1a762f85df78a35ddb88173147a582
KSH-a <kimjune.net@gmail.com>
2025-04-26 15:31:54 +0900

    commit : TestCode

---
+++ src/test/java/org/example/broong/domain/menu/service/MenuServiceTest.java
@@ -0,0 +1,14 @@
+package org.example.broong.domain.menu.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MenuServiceTest {
+
+    @Test
+    @DisplayName("test코드 작성")
+    void CreateServiceTest(){
+
+
+    }
+}
