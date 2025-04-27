44d9fc81a71a1f42d8504ff6756e2c980a1ad7b5
Deabaind <167089259+Deabaind@users.noreply.github.com>
2025-04-27 18:16:09 +0900

    Rollback to menu (#33)
    * Reapply "fix: 예외처리 수정 (#29)"
    
    This reverts commit 816725f2b281c8eefdb16831f159e54d04c6a199.
    
    * Revert "Feat/orders (#25)"
    
    This reverts commit 60f333f20be85012b80b4dce033ee795a5612065.

--- src/main/java/org/example/broong/domain/orderItem/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.broong.domain.orderItem.repository;
-
-import org.example.broong.domain.orderItem.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
